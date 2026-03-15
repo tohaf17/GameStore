2619441af653f3c335a4693f5c5e51ae8a1f8139
anton <filipenkoa270@gmail.com>
2026-03-15 19:32:53 +0200

    Додано TotalGamesHandlerMiddleware

---
+++ GameStore/GameStoreApi/Middlewares/TotalGamesHandlerMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GameStore.Infrastructure.Data;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace GameStoreApi.Middlewares
+{
+    public class TotalGamesHandlerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IMemoryCache cache;
+        public TotalGamesHandlerMiddleware(RequestDelegate next, IMemoryCache cache)
+        {
+            this.next = next;
+            this.cache = cache;
+        }
+        public async Task InvokeAsync(HttpContext context, GameStoreContext db)
+        {
+            if (!cache.TryGetValue("TotalGamesNumber", out string? totalGamesNumber))
+            {
+                totalGamesNumber = db.Games.CountAsync().ToString();
+                cache.Set("TotalGamesNumber", totalGamesNumber, TimeSpan.FromMinutes(1));
+            }
+            context.Response.Headers.Append("x-total-numbers-of-games", totalGamesNumber);
+            await Task.CompletedTask;
+
+            await next(context);
+
+        }
+    }
+}
